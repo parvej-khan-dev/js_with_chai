b997dae070325a92aa3663593b7dca869818a319
Parvej khan <parvejkhan@apples-MacBook-Air.local>
2023-12-07 23:08:24 +0530

    conditional flow js

---
+++ control_flow/01_basic.js
@@ -0,0 +1,35 @@
+// If else and swicht case
+
+/*
+if(condition){
+    //scope 
+}
+
+*/
+
+// example
+
+// for single condition
+const isUserLogin = true;
+
+if (isUserLogin) {
+  console.log("user login Successfully");
+}
+
+// for multiple condition
+let month = 1;
+
+if (month == 1) {
+  console.log("January");
+} else if (month == 2) {
+  console.log("February");
+} else {
+  console.log("Invalid Month");
+}
+// if else condition
+
+if (month == 1) {
+  console.log("January");
+} else {
+  console.log("Invalid Month");
+}
---
+++ control_flow/02_switch_case.js
@@ -0,0 +1,11 @@
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+// switch case statement syntax
+
