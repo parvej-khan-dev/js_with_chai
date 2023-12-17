20caa9ca45f1a86109a5f34affdec44df291ff46
Parvej khan <parvejkhan@apples-MacBook-Air.local>
2023-12-17 18:23:31 +0530

    loop completed

---
+++ loops/while_loop.js
@@ -0,0 +1,26 @@
+
+let index = 0;
+
+while (index <=20) {
+    // console.log(`value of index ${index}`);
+    index++
+}
+
+
+// loop on array 
+
+let sampleArray = [1,2,3,4,5,6,7,8,9,10];
+let i = 0
+while(i < sampleArray.length){
+    // console.log("Array is ::",sampleArray[i])
+    i++
+
+}
+
+// do while loop 
+let score = 11;
+
+do {
+    console.log(`score value is ${score}`)
+    score++
+} while (score <=10);
